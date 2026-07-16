3ac8a29194cf5f855ce7cb4d1b6c1c2955360841
Alejandro Robalino Zaldumbide <abrobalino@espe.edu.ec>
2026-07-16 10:10:34 -0500

    Implementación de la clase CruiseControl según la especificación

--- src/main/java/es/upm/grise/cruiseControl/CruiseControl.java
+++ src/main/java/es/upm/grise/cruiseControl/CruiseControl.java
@@ -1,5 +1,7 @@
 package es.upm.grise.cruiseControl;
 
+import es.upm.grise.cruiseControl.exceptions.*;
+
 public class CruiseControl {
 	
 	private RoadInformation roadInformation;
@@ -12,25 +14,99 @@ public class CruiseControl {
 	 * Constructor
 	 */
 	
-	public CruiseControl(RoadInformation roadInformation, Speedometer speedometer) {}
+	public CruiseControl(RoadInformation roadInformation, Speedometer speedometer) {
+		this.roadInformation = roadInformation;
+		this.speedometer = speedometer;
+		this.speedLimit = null;
+		this.speedSet = null;
+		this.enabled = false;
+	}
 	
 	/*
 	 * Method to code/test
 	 */
 	
-	public void setSpeedSet(int speedSet) {}
+	public void setSpeedSet(int speedSet) throws IncorrectSpeedSetException, SpeedSetAboveSpeedLimitException {
+		if (speedSet <= 0) {
+			throw new IncorrectSpeedSetException();
+		} else {
+			if (this.speedLimit != null) {
+				if (speedSet > this.speedLimit) {
+					throw new SpeedSetAboveSpeedLimitException();
+				} else {
+					this.speedSet = speedSet;
+					this.enabled = true;
+				}
+			} else {
+				this.speedSet = speedSet;
+				this.enabled = true;
+			}
+		}
+	}
 	
 	/* 
 	 * Method to code/test
 	 */
 	
-	public void setSpeedLimit(int speedLimit) {}
+	public void setSpeedLimit(int speedLimit) throws IncorrectSpeedLimitException, CannotSetSpeedLimitException {
+		if (speedLimit <= 0) {
+			throw new IncorrectSpeedLimitException();
+		} else {
+			if (this.speedSet != null) {
+				throw new CannotSetSpeedLimitException();
+			} else {
+				this.speedLimit = speedLimit;
+			}
+		}
+	}
 	
 	/* 
 	 * Method to code/test
 	 */
 	
-	public void disable() {}
+	public void disable() {
+		this.enabled = false;
+		this.speedSet = null;
+	}
+
+	/*
+	 * Method to code/test
+	 */
+	public Response nextCommand() {
+		Response response = new Response();
+		
+		if (speedSet == null) {
+			response.command = Command.IDLE;
+		} else if (enabled == false) {
+			response.command = Command.IDLE;
+		} else {
+			int currentSpeed = speedometer.getCurrentSpeed();
+			int maxSpeed = roadInformation.getMaxSpeed();
+			int minSpeed = roadInformation.getMinSpeed();
+			
+			if (currentSpeed < minSpeed) {
+				response.command = Command.INCREASE;
+			} else {
+				if (currentSpeed > maxSpeed) {
+					response.command = Command.REDUCE;
+				} else {
+					if (currentSpeed > speedSet) {
+						response.command = Command.REDUCE;
+					} else {
+						if (currentSpeed < speedSet) {
+							response.command = Command.INCREASE;
+						} else {
+							if (currentSpeed == speedSet) {
+								response.command = Command.KEEP;
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return response;
+	}
 	
 	/* 
 	 * Others getters and setters
